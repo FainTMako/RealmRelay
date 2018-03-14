d3b9e89efe4081fceb32fcae281d0be781e48c65
ft <ft@ft-PC>
2018-03-14 10:12:21 -0400

    Added constructor to LocationRecord.

--- src/main/java/realmrelay/data/LocationRecord.java
+++ src/main/java/realmrelay/data/LocationRecord.java
@@ -8,6 +8,22 @@ import java.io.IOException;
 public class LocationRecord extends Location {
 	
 	public int time;
+
+        public LocationRecord()
+        {
+        }
+
+    public LocationRecord(int _arg_1, double _arg_2, double _arg_3)
+    {
+        this.time = _arg_1;
+        this.x = (float) _arg_2;
+        this.y = (float) _arg_3;
+    }
+        
+        
+        
+        
+        
 	
 	@Override
 	public void parseFromInput(DataInput in) throws IOException {
